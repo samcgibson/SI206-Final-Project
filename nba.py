0f505c87e5c62c5bfa46d82bb88339465c1c7155
samcgibson <samcg@umich.edu>
2023-04-05 16:33:50 -0400

    implemented code to add 25 rows to db at a time

--- nba.py
+++ nba.py
@@ -27,29 +27,39 @@ cur, conn = open_database('NBA.db')
 # cur.execute("DROP TABLE Games")
 
 
-# def make_games_table(list, cur, conn):
-#     cur.execute("CREATE TABLE IF NOT EXISTS Games (game_id INTEGER PRIMARY KEY, day INTEGER, time INTEGER, home_team_id INTEGER, away_team_id INTEGER, winner_id INTEGER, score_diff INTEGER)")
-#     for gameId in list:
-#         box = boxscore.BoxScore(gameId)
-#         data = box.get_dict()
-#         gid = int(data['game']['gameId'])
-#         date = data['game']['gameTimeLocal'].split("T")[0]
-#         date = int(date.replace('-', ''))
-#         time = data['game']['gameTimeLocal'].split("T")[1]
-#         time = time.split('-')[0]
-#         time = int(time.replace(':', '')[:-2])
-#         homeid = data['game']['homeTeam']['teamId']
-#         awayid = data['game']['awayTeam']['teamId']
+def make_games_table(list, cur, conn):
+    cur.execute("CREATE TABLE IF NOT EXISTS Games (game_id INTEGER PRIMARY KEY, day INTEGER, time INTEGER, home_team_id INTEGER, away_team_id INTEGER, winner_id INTEGER, score_diff INTEGER)")
 
-#         if data['game']['homeTeam']['score'] > data['game']['awayTeam']['score']:
-#             winnerid = homeid
-#             scorediff = data['game']['homeTeam']['score'] - data['game']['awayTeam']['score']
-#         else:
-#             winnerid = awayid
-#             scorediff = data['game']['awayTeam']['score'] - data['game']['homeTeam']['score']
+    cur.execute("SELECT COUNT(*) FROM Games")
+    result = cur.fetchone()[0]
+
+    for i in range(result, result + 25):
+        if i >= len(list):
+            break
+        box = boxscore.BoxScore(list[i])
+        data = box.get_dict()
+        gid = int(data['game']['gameId'])
+        date = data['game']['gameTimeLocal'].split("T")[0]
+        date = int(date.replace('-', ''))
+        time = data['game']['gameTimeLocal'].split("T")[1]
+        time = time.split('-')[0]
+        time = int(time.replace(':', '')[:-2])
+        homeid = data['game']['homeTeam']['teamId']
+        awayid = data['game']['awayTeam']['teamId']
+
+        if data['game']['homeTeam']['score'] > data['game']['awayTeam']['score']:
+            winnerid = homeid
+            scorediff = data['game']['homeTeam']['score'] - data['game']['awayTeam']['score']
+        else:
+            winnerid = awayid
+            scorediff = data['game']['awayTeam']['score'] - data['game']['homeTeam']['score']
         
-#         cur.execute("INSERT OR IGNORE INTO Games (game_id, day, time, home_team_id, away_team_id, winner_id, score_diff) VALUES (?, ?, ?, ?, ?, ?, ?)", (gid, date, time, homeid, awayid, winnerid, scorediff))
-#     conn.commit()
+        cur.execute("INSERT OR IGNORE INTO Games (game_id, day, time, home_team_id, away_team_id, winner_id, score_diff) VALUES (?, ?, ?, ?, ?, ?, ?)", (gid, date, time, homeid, awayid, winnerid, scorediff))
+        print(f'Row {i + 1} inserted.')
+    
+    conn.commit()
+
+make_games_table(gameIdList, cur, conn)
 
 # def make_teams_table(cur, conn):
 #     teamlist = teams.get_teams()
@@ -60,16 +70,15 @@ cur, conn = open_database('NBA.db')
 #     conn.commit()
 
 # make_teams_table(cur, conn)
-# make_games_table(gameIdList, cur, conn)
 
-def make_players_table(cur, conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS Players (player_id INTEGER PRIMARY KEY, player_name TEXT UNIQUE)")
-    playerlist = players.get_players()
-    for player in playerlist:
-        if player['is_active'] == True:
-            cur.execute("INSERT OR IGNORE INTO Players (player_id, player_name) VALUES (?, ?)", (player['id'], player['full_name']))
+# def make_players_table(cur, conn):
+#     cur.execute("CREATE TABLE IF NOT EXISTS Players (player_id INTEGER PRIMARY KEY, player_name TEXT UNIQUE)")
+#     playerlist = players.get_players()
+#     for player in playerlist:
+#         if player['is_active'] == True:
+#             cur.execute("INSERT OR IGNORE INTO Players (player_id, player_name) VALUES (?, ?)", (player['id'], player['full_name']))
 
-    conn.commit()
+#     conn.commit()
 
 
-make_players_table(cur, conn)
+# make_players_table(cur, conn)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,8 @@
 from nba_api.live.nba.endpoints import playbyplay
 from nba_api.live.nba.endpoints import boxscore
 from nba_api.stats.static import teams
+from nba_api.stats.static import players
+
 
 import time
 import re
@@ -8,29 +10,12 @@ import sqlite3
 import json
 import os
 
-october18 = '0022200001' # first game of the regular season october 18th, 2022
-january1 = '022200547'
-january31 = '0022200768'
 february1 = '0022200770'
 february28 = '0022200932'
-march22 = '0022201092' # last game on march 22nd, 2023
-#test
 
 gameIdList = []
 for id in range(int(february1), int(february28) + 1): # add game IDs to gamelist
     gameIdList.append("00" + str(id)) # have to add '00' here because integers don't allow leading 0's
-
-firstbucketsdict = {}
-firstbucketslist = []
-
-# for gameId in gameIdList:
-#     pbp = playbyplay.PlayByPlay(gameId)
-#     pbpdict = pbp.get_dict()
-#     pbpjson = pbp.get_json()
-    
-    # box = boxscore.BoxScore(gameId)
-    # bdict = box.get_dict()
-    # bjson = box.get_json()
 
 def open_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
