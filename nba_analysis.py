fc6f77f0924be6e0d2deb9a5a05e6e5ea40bc106
samcgibson <samcg@umich.edu>
2023-04-06 11:03:35 -0400

    made graph

--- nba_analysis.py
+++ nba_analysis.py
@@ -12,21 +12,38 @@ path = os.path.dirname(os.path.abspath(__file__))
 conn = sqlite3.connect(path+'/'+'NBA.db')
 cur = conn.cursor()
 
-cur.execute("SELECT FirstBuckets.player_id, Players.player_name, FirstBuckets.shot_distance, FirstBuckets.points FROM FirstBuckets JOIN Players ON FirstBuckets.player_id = Players.player_id")
+# cur.execute("SELECT FirstBuckets.player_id, Players.player_name, FirstBuckets.shot_distance, FirstBuckets.points, FirstBuckets.team_id FROM FirstBuckets JOIN Players ON FirstBuckets.player_id = Players.player_id")
 
-firstbucketlist = []
+# firstbucketlist = []
+# for row in cur:
+#     print(row)
+#     firstbucketlist.append((row[1], row[2]))
+
+# firstbucketdict = dict(firstbucketlist)
+
+cur.execute("SELECT DISTINCT Players.player_name, Games.game_id, FirstBuckets.team_id, Games.winner_id, FirstBuckets.shot_distance, FirstBuckets.points, Teams.team_name "
+            "FROM FirstBuckets JOIN Players ON FirstBuckets.player_id = Players.player_id "
+            "JOIN Games ON FirstBuckets.game_id = Games.game_id "
+            "JOIN Teams ON FirstBuckets.team_id = Teams.team_id ")
+
+tuples = []
+
 for row in cur:
-    firstbucketlist.append((row[1], row[2]))
+    if row[2] == row[3]:
+        tuples.append(row + ('won',))
+    else:
+        tuples.append(row + ('lost',))
 
-firstbucketdict = dict(firstbucketlist)
+# print(tuples)
 
-print(firstbucketdict)
+
 
 # firstbucketdict = dict(Counter(firstbucketlist))
 
 # sorted_dict = dict(sorted(firstbucketdict.items(), key=lambda x: x[1], reverse=True))
 
-# my_df = pd.DataFrame(firstbucketdict.items())
-# sb.scatterplot(x=0, y=1, data=my_df)
+df = pd.DataFrame(tuples)
 
-# plt.show()
+sb.lineplot(data=df, x=1, y = 4, hue=7)
+
+plt.show()
