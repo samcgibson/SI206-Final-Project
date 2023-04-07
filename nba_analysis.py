120e501c30c0b76988f826163d16f984068d504f
samcgibson <samcg@umich.edu>
2023-04-06 21:14:48 -0400

    improved make_conversion_graph()

--- nba_analysis.py
+++ nba_analysis.py
@@ -62,67 +62,78 @@ cur = conn.cursor()
 
 # make_basic_charts(cur)
 
-def make_shotchart(cur):
-    cur.execute("SELECT FirstBuckets.xpos, FirstBuckets.ypos, FirstBuckets.game_id, FirstBuckets.team_id, Games.home_team_id FROM FirstBuckets JOIN Games ON FirstBuckets.game_id = Games.game_id")
+# def make_shotchart(cur):
+#     cur.execute("SELECT FirstBuckets.xpos, FirstBuckets.ypos, FirstBuckets.game_id, FirstBuckets.team_id, Games.home_team_id FROM FirstBuckets JOIN Games ON FirstBuckets.game_id = Games.game_id")
 
-    tuples = []
-
-    for row in cur:
-        if row[3] == row[4]:
-            tuples.append(row + ('Home', ))
-        if row[3] != row[4]:
-            tuples.append(row + ('Away', ))
-
-    ftpos_home = {'xpos': 20.21, 'ypos': 50}
-    ftpos_away = {'xpos': 79.79, 'ypos': 50}
-
-    df = pd.DataFrame(tuples, columns=['xpos', 'ypos', 'game_id', 'team_id', 'hometeam_id', 'Status'])
-    pd.set_option('display.max_rows', 1000)
-
-    df.loc[df['Status'] == 'Home', ['xpos', 'ypos']] = df.loc[df['Status'] == 'Home', ['xpos', 'ypos']].fillna(value=ftpos_home)
-    df.loc[df['Status'] == 'Away', ['xpos', 'ypos']] = df.loc[df['Status'] == 'Away', ['xpos', 'ypos']].fillna(value=ftpos_away)
-
-    sb.set(rc={"figure.figsize":(9.4, 5)})
-
-    g = sb.scatterplot(x = 'xpos', y = 'ypos', s=80, hue = 'Status', data=df, palette=['#1D42BA', '#C8102E'])
-    ax = plt.gca()
-
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.xlabel('94 ft.')
-    plt.ylabel('50 ft.')
-    plt.title('NBA First Basket (Made) Shot Chart')
-    plt.show()
-
-make_shotchart(cur)
-
-# def make_conversion_graph(cur):
-#     cur.execute("SELECT DISTINCT Games.day, Teams.team_name, FirstBuckets.team_id, Games.winner_id, Games.score_diff "
-#                 "FROM Games JOIN FirstBuckets ON Games.game_id = FirstBuckets.game_id "
-#                 "JOIN Teams ON Teams.team_id = FirstBuckets.team_id "  
-#                 "ORDER BY Games.day ")
 #     tuples = []
 
 #     for row in cur:
-#         if row[2] == row[3]:
-#             tuples.append(row + ('Won',))
-#         else:
-#             tuples.append(row + ('Lost',))
+#         if row[3] == row[4]:
+#             tuples.append(row + ('Home', ))
+#         if row[3] != row[4]:
+#             tuples.append(row + ('Away', ))
 
-#     df = pd.DataFrame(tuples, columns=['day', 'team', 'team_id', 'winner_id', 'Score Difference', 'Result'])
+#     ftpos_home = {'xpos': 20.21, 'ypos': 50}
+#     ftpos_away = {'xpos': 79.79, 'ypos': 50}
 
-#     g = sb.scatterplot(data = df, x = 'day', y = 'team', hue = 'Result', size = 'Score Difference', sizes=(20, 300), alpha=.75, palette="muted", legend='brief')
-#     sb.move_legend(
-#         g, "lower center",
-#         bbox_to_anchor=(.5, .975), ncol=3, title=None, frameon=False,
-# )
-#     plt.xlabel('Games in February 2023 (by date)')
-#     plt.ylabel('Team')
+#     df = pd.DataFrame(tuples, columns=['xpos', 'ypos', 'game_id', 'team_id', 'hometeam_id', 'Status'])
+#     pd.set_option('display.max_rows', 1000)
 
-#     plt.grid()
+#     df.loc[df['Status'] == 'Home', ['xpos', 'ypos']] = df.loc[df['Status'] == 'Home', ['xpos', 'ypos']].fillna(value=ftpos_home)
+#     df.loc[df['Status'] == 'Away', ['xpos', 'ypos']] = df.loc[df['Status'] == 'Away', ['xpos', 'ypos']].fillna(value=ftpos_away)
+
+#     sb.set(rc={"figure.figsize":(9.4, 5)})
+
+#     g = sb.scatterplot(x = 'xpos', y = 'ypos', s=80, hue = 'Status', data=df, palette=['#1D42BA', '#C8102E'])
+#     ax = plt.gca()
+
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+#     plt.xlabel('94 ft.')
+#     plt.ylabel('50 ft.')
+#     plt.title('NBA First Basket (Made) Shot Chart')
 #     plt.show()
 
-# make_conversion_graph(cur)
+# make_shotchart(cur)
+
+def make_conversion_graph(cur):
+    cur.execute("SELECT DISTINCT Games.day, Teams.team_name, FirstBuckets.team_id, Games.winner_id, Games.score_diff "
+                "FROM Games JOIN FirstBuckets ON Games.game_id = FirstBuckets.game_id "
+                "JOIN Teams ON Teams.team_id = FirstBuckets.team_id "  
+                "ORDER BY Games.day ")
+    tuples = []
+
+    for row in cur:
+        if row[2] == row[3]:
+            tuples.append(row + ('Won',))
+        else:
+            tuples.append(row + ('Lost',))
+
+    df = pd.DataFrame(tuples, columns=['day', 'team', 'team_id', 'winner_id', 'Score Difference', 'Result'])
+
+    sb.set(rc={"figure.figsize":(12, 10)})
+
+    g = sb.scatterplot(data = df, x = 'day', y = 'team', hue = 'Result', size = 'Score Difference', sizes=(20, 300), alpha=.75, palette=["#1D42BA", "#C8102E"], legend='brief')
+    g.legend_.set_title(None)
+    g.legend_.texts[0].set_text('Lost')
+    g.legend_.texts[1].set_text('Won')
+    g.legend_.get_frame().set_linewidth(0.0)
+    g.legend_.get_frame().set_edgecolor('gray')
+    g.legend_.get_frame().set_facecolor('white')
+    g.legend_.get_frame().set_alpha(0.6)
+    g.legend_.shadow = True
+    
+    plt.grid()
+    plt.xlabel('Games in February 2023 (by date)')
+    plt.ylabel('Team')
+    plt.suptitle('Teams Ability to Win After Getting First Basket')
+    plt.title('Size of circle is how much team won / lost by. The large gap in data is during All-Star Weekend.')
+    plt.tight_layout()
+    plt.grid()
+    plt.show()
+
+make_conversion_graph(cur)
+
 
 # def make_shotdistance_graph(cur):
 
