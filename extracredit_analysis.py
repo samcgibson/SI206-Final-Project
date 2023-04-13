d31e7c941761d2c12d335e98679b991c94b0d0fb
samcgibson <samcg@umich.edu>
2023-04-13 10:29:33 -0400

    added avg lines to extra credit graph

--- extracredit_analysis.py
+++ extracredit_analysis.py
@@ -19,9 +19,16 @@ def make_charts(cur):
     
     df = pd.DataFrame(cur, columns=['Day', 'page', 'Word Count', 'Print Status'])
 
-    sb.scatterplot(data=df, x = df['Day'], y = df['Word Count'], hue = df['Print Status'])
+    sb.scatterplot(data=df, x=df['Day'], y=df['Word Count'], hue=df['Print Status'])
     plt.title('NYT Sports Articles, Feb. 2023')
     plt.tight_layout()
+
+    mean_printed = df[df['Print Status'] == 'Printed']['Word Count'].mean()
+    mean_not_printed = df[df['Print Status'] != 'Printed']['Word Count'].mean()
+
+    plt.axhline(mean_printed, linestyle='--', color='#4372C4')
+    plt.axhline(mean_not_printed, linestyle='--', color='#ED7D32')
+
     plt.show()
 
 make_charts(cur)
